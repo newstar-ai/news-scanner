92d16db3393e9e9d5cad6c835018b918a7a73c90
ltta18 <thuylinh7720@gmail.com>
2020-10-02 17:00:22 +0700

    Custom Highlight

--- frontend/src/components/Highlight.js
+++ frontend/src/components/Highlight.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import Highlighter from 'react-highlight-words';
 
 const Highlight = ({ searchInput, text }) => {
-  return (
-    <Highlighter
-      highlightClassName="hl-class"
-      searchWords={[searchInput]}
-      textToHighlight={text}
-      autoEscape
-    />
-  );
+    const createHighlight = () => {
+        text = ' ' + text
+
+        const querystr = `[., *+\-?^\${}()|[\\]]+${searchInput}[., *+\-?^\${}()|[\\]]+`;
+        const reg = new RegExp(querystr, 'gim');
+        const finalStr = `${text.replace(reg, function(str) {return `${str[0]}<mark class="hl-class">${str.slice(1, str.length-1)}</mark>${str[str.length-1]}`})}`;
+        return finalStr;
+    }
+
+    return (
+        <span dangerouslySetInnerHTML={{__html: createHighlight()}}></span>
+    );
 };
 
 export default Highlight;
--- frontend/src/components/SearchHome/ResultItem.js
+++ frontend/src/components/SearchHome/ResultItem.js
@@ -1,68 +1,67 @@
 import { List } from 'antd';
 import Moment from 'moment';
 import React from 'react';
-import Highlighter from 'react-highlight-words';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 // import sgnews from '../images/sgnews.png';
 import '../../css/Result.css';
+import Highlight from '../Highlight';
 const ResultItem = ({
-  id,
-  title,
-  author,
-  content,
-  link,
-  pageNum,
-  pubDate,
-  newsTitle
+    id,
+    title,
+    author,
+    content,
+    link,
+    pageNum,
+    pubDate,
+    newsTitle
 }) => {
-  const { highlightText, searchFilter } = useSelector(state => state.search);
+    const { highlightText, searchFilter } = useSelector(state => state.search);
 
-  return (
-    <List.Item
-      style={{ padding: 0 }}
-      key={id}
-      extra={<img width={272} height={168} alt="logo" src={'http://' + link} />}
-    >
-      <List.Item.Meta
-        // avatar={<Avatar src={item.avatar} />}
-        title={
-          <Link to={{ pathname: `article/${id}` }}>
-            <Highlighter
+    return (
+        <List.Item
+            style={{ padding: 0 }}
+            key={id}
+            extra={<img width={272} height={168} alt="logo" src={'http://' + link} />}
+        >
+            <List.Item.Meta
+                // avatar={<Avatar src={item.avatar} />}
+                title={
+                    <Link to={{ pathname: `article/${id}` }}>
+                        {/* <Highlighter
               highlightClassName="hl-class"
               searchWords={
                 searchFilter.includes('title') ? [highlightText] : []
               }
               textToHighlight={title}
               autoEscape
-            />
-          </Link>
-        }
-        description={
-          <>
-            <p>
-              #{pageNum} - {Moment(pubDate).format('DD MMM YYYY')} - {newsTitle}
-              <br></br>
-              <Highlighter
-                highlightClassName="hl-class"
-                searchWords={
-                  searchFilter.includes('author') ? [highlightText] : []
+            /> */}
+                        <Highlight searchInput={
+                            searchFilter.includes('title') ? highlightText : ''
+                        }
+                        text={title} />
+                    </Link>
                 }
-                textToHighlight={author}
-                autoEscape
-              />
-            </p>
-          </>
-        }
-      />
-      <Highlighter
-        highlightClassName="hl-class"
-        searchWords={searchFilter.includes('content') ? [highlightText] : []}
-        textToHighlight={content}
-        autoEscape
-      />
-    </List.Item>
-  );
+                description={
+                    <>
+                        <p>#{pageNum} - {Moment(pubDate).format('DD MMM YYYY')} - {newsTitle}
+                            <br></br>
+                            <Highlight 
+                                searchInput={
+                                    searchFilter.includes('author') ? highlightText : ''
+                                }
+                                text={author}
+                            />
+                        </p>
+                    </>
+                }
+            />
+            <Highlight searchInput={
+                searchFilter.includes('content') ? highlightText : ''
+            }
+            text={content} />
+        </List.Item>
+    );
 };
 
 export default ResultItem;
