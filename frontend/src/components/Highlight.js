42827f16f9f89322be12d48fdb12247f84239812
Tran Trong Hung <tronghung199@gmail.com>
2020-10-02 12:55:21 +0700

    update

---
+++ frontend/src/components/Highlight.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Highlighter from 'react-highlight-words';
+
+const Highlight = ({ searchInput, text }) => {
+  return (
+    <Highlighter
+      highlightClassName="hl-class"
+      searchWords={[searchInput]}
+      textToHighlight={text}
+      autoEscape
+    />
+  );
+};
+
+export default Highlight;
